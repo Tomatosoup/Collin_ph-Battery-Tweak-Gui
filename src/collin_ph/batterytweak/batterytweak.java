118bcb97b380ba10b4305ebfd86964fa4c87e63d
Tomatosoup <ccsvp1993@gmail.com>
2010-08-10 20:19:22 -0400

    first commit

---
+++ src/collin_ph/batterytweak/batterytweak.java
@@ -0,0 +1,108 @@
+package collin_ph.batterytweak;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+
+public class batterytweak extends Activity
+{
+private ListView lv1;
+private String lv_arr[]={"Revert to 768mhz defaults","Revert to 710mhz defaults","Customize Settings","Toggle Battery Tweak","Toggle CFS Tweaks","Toggle LED Fix","Toggle New Disk Boot","Toggle Audio Quality Fix"};
+private int battweak;
+private int cfs;
+private int led;
+private int disk;
+private int audio;
+public void onCreate(Bundle icicle){
+battweak = 1;
+cfs = 1;
+led = 1;
+disk = 1;
+audio = 1;
+super.onCreate(icicle);
+setContentView(R.layout.main);
+lv1=(ListView)findViewById(R.id.ListView01);
+lv1.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1 , lv_arr));
+lv1.setTextFilterEnabled(true);
+lv1.setOnItemClickListener(new OnItemClickListener() {
+private int cobbles;
+private int cfs;
+
+public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+cobbles = 0;
+if (lv1.getItemAtPosition(position) == "Revert to 768mhz defaults"){
+	Toast.makeText( getApplicationContext() , "Reverted to 768mhz defaults", 
+	Toast.LENGTH_SHORT).show();
+}
+if (lv1.getItemAtPosition(position) == "Revert to 710mhz defaults"){
+	Toast.makeText( getApplicationContext() , "Reverted to 710mhz defaults", 
+	Toast.LENGTH_SHORT).show();
+}
+if (lv1.getItemAtPosition(position) == "Customize Settings"){
+	Toast.makeText( getApplicationContext() , "Fill this space later", 
+	Toast.LENGTH_SHORT).show();
+}
+if (lv1.getItemAtPosition(position) == "Toggle Battery Tweak" && battweak == 1){
+	Toast.makeText( getApplicationContext() , "Battery Tweak Diabled", 
+	Toast.LENGTH_SHORT).show();
+	battweak = 0;
+	cobbles = 1;
+}
+if (lv1.getItemAtPosition(position) == "Toggle Battery Tweak" && battweak == 0 && cobbles == 0){
+	Toast.makeText( getApplicationContext() , "Battery Tweak Enabled", 
+	Toast.LENGTH_SHORT).show();
+	battweak = 1;
+}
+if (lv1.getItemAtPosition(position) == "Toggle CFS Tweaks" && cfs == 1){
+	Toast.makeText( getApplicationContext() , "CFS Tweaks Diabled", 
+	Toast.LENGTH_SHORT).show();
+	cfs = 0;
+	cobbles = 1;
+}
+if (lv1.getItemAtPosition(position) == "Toggle CFS Tweaks" && cfs == 0 && cobbles == 0){
+	Toast.makeText( getApplicationContext() , "CFS Tweaks Enabled", 
+	Toast.LENGTH_SHORT).show();
+	cfs = 1;
+}
+if (lv1.getItemAtPosition(position) == "Toggle LED Fix" && led == 1){
+	Toast.makeText( getApplicationContext() , "LED Fix Diabled", 
+	Toast.LENGTH_SHORT).show();
+	led = 0;
+	cobbles = 1;
+}
+if (lv1.getItemAtPosition(position) == "Toggle LED Fix" && led == 0 && cobbles == 0){
+	Toast.makeText( getApplicationContext() , "LED Fix Enabled", 
+	Toast.LENGTH_SHORT).show();
+	led = 1;
+}
+if (lv1.getItemAtPosition(position) == "Toggle New Disk Boot" && disk == 1){
+	Toast.makeText( getApplicationContext() , "New Disk Boot Diabled", 
+	Toast.LENGTH_SHORT).show();
+	disk = 0;
+	cobbles = 1;
+}
+if (lv1.getItemAtPosition(position) == "Toggle New Disk Boot" && disk == 0 && cobbles == 0){
+	Toast.makeText( getApplicationContext() , "New Disk Boot Enabled", 
+	Toast.LENGTH_SHORT).show();
+	disk = 1;
+}
+if (lv1.getItemAtPosition(position) == "Toggle Audio Quality Fix" && audio == 1){
+	Toast.makeText( getApplicationContext() , "Audio Quality Fix Diabled", 
+	Toast.LENGTH_SHORT).show();
+	audio = 0;
+	cobbles = 1;
+}
+if (lv1.getItemAtPosition(position) == "Toggle Audio Quality Fix" && audio == 0 && cobbles == 0){
+	Toast.makeText( getApplicationContext() , "Audio Quality Fix Enabled", 
+	Toast.LENGTH_SHORT).show();
+	audio = 1;
+}
+}
+});
+}
+}
