f30ded2ebb0f61e1f9de5d7895162e4400c9a812
Tomatosoup <ccsvp1993@gmail.com>
2010-08-12 00:59:50 -0400

    more stuff

--- src/collin_ph/batterytweak/batterytweak.java
+++ src/collin_ph/batterytweak/batterytweak.java
@@ -28,7 +28,12 @@ import android.widget.AdapterView.OnItemClickListener;
 public class batterytweak extends Activity
 {	
 private ListView lv1;
-private String audio;{
+private String audio;
+private String disk;
+private String cfs;
+private String led;
+private String batts;
+{
 	FileInputStream in = null;
 	try {
 		in = new FileInputStream("/system/etc/batt.conf");
@@ -53,14 +58,38 @@ private String audio;{
 		// TODO Auto-generated catch block
 		e.printStackTrace();
 	}
-	if (battconf[0]!= "audio_fix=1"){
+	if (battconf[0].equals("audio_fix=0")==true){
 		audio = "Enable Audio Fix";
 	}
-	if (battconf[0] != "audio_fix=0"){
+	if (battconf[0].equals("audio_fix=1")==true){
 		audio = "Disable Audio Fix";
 	}
+	if (battconf[1].equals("CFStweaks=0")==true){
+		cfs = "Enable CFS Tweaks";
+	}
+	if (battconf[1].equals("CFStweaks=1")==true){
+		cfs = "Disable CFS Tweaks";
+	}
+	if (battconf[2].equals("MOUNToptions=0")==true){
+		disk = "Enable New Disk Boot";
+	}
+	if (battconf[2].equals("MOUNToptions=1")==true){
+		disk = "Disable New Disk Boot";
+	}
+	if (battconf[3].equals("enabled=0")==true){
+		batts = "Enable Battery Tweak";
+	}
+	if (battconf[3].equals("enabled=1")==true){
+		batts = "Disable Battery Tweak";
+	}
+	if (battconf[4].equals("LEDfix=0")==true){
+		led = "Enable LED Fix";
+	}
+	if (battconf[4].equals("LEDfix=1")==true){
+		led = "Disable LED Fix";
+	}
 }
-private String lv_arr[]={"Revert to 768mhz defaults","Revert to 710mhz defaults","Customize Settings","Toggle Battery Tweak","Toggle CFS Tweaks","Toggle LED Fix","Toggle New Disk Boot",audio};
+private String lv_arr[]={"Revert to 768mhz defaults","Revert to 710mhz defaults","Customize Settings",batts,cfs,led,disk,audio};
 String[] battconf = new String[20];
 private String strLine;
 int batt;
@@ -72,12 +101,14 @@ public void onCreate(Bundle icicle){
 super.onCreate(icicle);
 setContentView(R.layout.main);
 lv1=(ListView)findViewById(R.id.ListView01);
-lv1.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1 , lv_arr));
-lv1Adapter = (new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1 , lv_arr));
+lv1Adapter=(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1 , lv_arr));
+lv1.setAdapter(lv1Adapter);
 lv1.setTextFilterEnabled(true);
 lv1.setOnItemClickListener(new OnItemClickListener() {
 private int cobbles;
 public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+lv1Adapter.notifyDataSetChanged();
+
 cobbles = 0;
 if (lv1.getItemAtPosition(position) == "Revert to 768mhz defaults"){
 	Toast.makeText( getApplicationContext() , "Reverted to 768mhz defaults", 
@@ -92,83 +123,71 @@ if (lv1.getItemAtPosition(position) == "Customize Settings"){
 	Toast.LENGTH_SHORT).show();
 }
 if (lv1.getItemAtPosition(position) == "Disable Battery Tweak"){
-	Toast.makeText( getApplicationContext() , "Battery Tweak Disabled", 
-	Toast.LENGTH_SHORT).show();
+	//Toast.makeText( getApplicationContext() , "Battery Tweak Disabled", 
+	//Toast.LENGTH_SHORT).show();
 	battconf[3] = "enabled=0";
+	lv_arr[3] = "Enable Battery Tweak";
 	cobbles = 1;
 }
 if (lv1.getItemAtPosition(position) == "Enable Battery Tweak" && cobbles == 0){
-	Toast.makeText( getApplicationContext() , "Battery Tweak Enabled", 
-	Toast.LENGTH_SHORT).show();
+	//Toast.makeText( getApplicationContext() , "Battery Tweak Enabled", 
+	//Toast.LENGTH_SHORT).show();
 	battconf[3] = "enabled=1";
+	lv_arr[3] = "Disable Battery Tweak";
 }
 if (lv1.getItemAtPosition(position) == "Disable CFS Tweaks"){
-	Toast.makeText( getApplicationContext() , "CFS Tweaks Diabled", 
-	Toast.LENGTH_SHORT).show();
-	battconf[1] = "CFStweaks=0";
+	//Toast.makeText( getApplicationContext() , "CFS Tweaks Diabled", 
+	//Toast.LENGTH_SHORT).show();
+	battconf[4] = "CFStweaks=0";
 	cobbles = 1;
+	lv_arr[4] = "Enable CFS Tweaks";
 }
 if (lv1.getItemAtPosition(position) == "Enable CFS Tweaks" && cobbles == 0){
-	Toast.makeText( getApplicationContext() , "CFS Tweaks Enabled", 
-	Toast.LENGTH_SHORT).show();
-	battconf[1] = "CFStweaks=1";
+	//Toast.makeText( getApplicationContext() , "CFS Tweaks Enabled", 
+	//Toast.LENGTH_SHORT).show();
+	battconf[4] = "CFStweaks=1";
+	lv_arr[4] = "Disable CFS Tweaks";
 }
 if (lv1.getItemAtPosition(position) == "Disable LED Fix"){
-	Toast.makeText( getApplicationContext() , "LED Fix Diabled", 
-	Toast.LENGTH_SHORT).show();
+	//Toast.makeText( getApplicationContext() , "LED Fix Diabled", 
+	//Toast.LENGTH_SHORT).show();
 	battconf[4] = "LEDfix=0";
 	cobbles = 1;
+	lv_arr[5] = "Enable LED Fix";
 }
 if (lv1.getItemAtPosition(position) == "Enable LED Fix" && cobbles == 0){
-	Toast.makeText( getApplicationContext() , "LED Fix Enabled", 
-	Toast.LENGTH_SHORT).show();
+	//Toast.makeText( getApplicationContext() , "LED Fix Enabled", 
+	//Toast.LENGTH_SHORT).show();
 	battconf[4] = "LEDfix=1";
+	lv_arr[5] = "Enable LED Fix";
 }
 if (lv1.getItemAtPosition(position) == "Disable New Disk Boot"){
-	Toast.makeText( getApplicationContext() , "New Disk Boot Diabled", 
-	Toast.LENGTH_SHORT).show();
+	//Toast.makeText( getApplicationContext() , "New Disk Boot Diabled", 
+	//Toast.LENGTH_SHORT).show();
 	battconf[2] = "MOUNToptions=0";
 	cobbles = 1;
+	lv_arr[6] = "Enable New Disk Boot";
 }
 if (lv1.getItemAtPosition(position) == "Enable New Disk Boot" && cobbles == 0){
-	Toast.makeText( getApplicationContext() , "New Disk Boot Enabled", 
-	Toast.LENGTH_SHORT).show();
+	//Toast.makeText( getApplicationContext() , "New Disk Boot Enabled", 
+	//Toast.LENGTH_SHORT).show();
 	battconf[2] = "MOUNToptions=1";
+	lv_arr[6] = "Disable New Disk Boot";
 }
 if (lv1.getItemAtPosition(position) == "Disable Audio Fix"){
-	Toast.makeText( getApplicationContext() , "Audio Quality Fix Diabled", 
-	Toast.LENGTH_SHORT).show();
+	//Toast.makeText( getApplicationContext() , "Audio Quality Fix Diabled", 
+	//Toast.LENGTH_SHORT).show();
 	battconf[0] = "audio_fix=0";
 	cobbles = 1;
+	lv_arr[7] = "Enable Audio Fix";
 }
 if (lv1.getItemAtPosition(position) == "Enable Audio Fix" && cobbles == 0){
-	Toast.makeText( getApplicationContext() , "Audio Quality Fix Enabled", 
-	Toast.LENGTH_SHORT).show();
+	//Toast.makeText( getApplicationContext() , "Audio Quality Fix Enabled", 
+	//Toast.LENGTH_SHORT).show();
 	battconf[0] = "audio_fix=1";
+	lv_arr[7] = "Disable Audio Fix";
 }
-try {
-	BufferedWriter out = new BufferedWriter(new FileWriter("/system/etc/batt2.conf"));
-	out.write(battconf[0]);
-	out.write(battconf[1]);
-	out.write(battconf[2]);
-	out.write(battconf[3]);
-	out.write(battconf[4]);
-	out.write(battconf[5]);
-	out.write(battconf[6]);
-	out.write(battconf[7]);
-	out.write(battconf[8]);
-	out.write(battconf[9]);
-	out.write(battconf[10]);
-	out.write(battconf[11]);
-	out.write(battconf[12]);
-	out.write(battconf[13]);
-	out.write(battconf[14]);
-	out.write(battconf[15]);
-	out.write(battconf[16]);
 
-	out.close();
-	} catch (IOException e) {
-	}
 	
 }
 });
